add315a24646d6305f058d8e197b6ee3b03ebd41
Seth Reid <contact@sethreid.co.nz>
2017-02-26 12:30:48 +1300

    moved into src folder and made generic between platforms

---
+++ docs/presentation-notes.md
@@ -0,0 +1,17 @@
+# Presentation Notes
+
+- Accessibility
+
+https://facebook.github.io/react-native/docs/accessibility.html
+
+- Navigator
+
+https://facebook.github.io/react-native/docs/navigator.html
+
+- Dev tools
+
+    - Reload
+    - Hot reload
+    - Reactotron
+    - Debuging
+    
---
+++ src/root.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class RootComponent extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Welcome to React Native!
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+});
