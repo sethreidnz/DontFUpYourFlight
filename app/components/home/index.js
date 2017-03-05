cf4ecb5ed87623358990a6d2cf81d305530ddb98
Seth Reid <contact@sethreid.co.nz>
2017-03-05 15:37:38 +1300

    implemented log out

--- app/components/home/index.js
+++ app/components/home/index.js
@@ -1,19 +1,27 @@
 import React, { Component, PropTypes } from 'react'
-import {
-  StyleSheet
-} from 'react-native'
+import { navigateTo } from '../../modules/utility'
 
 import HomeActionButtons from './components/HomeActionButtons'
 
 export default class HomeScreen extends Component {
   static propTypes = {
-    navigation: PropTypes.object.isRequired
+    actions: PropTypes.shape({
+      logoutUser: PropTypes.func.required
+    }),
+    navigation: PropTypes.object.isRequired,
+    isLoggedIn: PropTypes.bool.isRequired
   }
   static navigationOptions = {
     title: 'Dont F Up Your Flight'
   }
+  componentWillReceiveProps = (nextProps) => {
+    const { isLoggedIn, navigation } = nextProps
+    if (!isLoggedIn) {
+      navigateTo(navigation, 'AuthScreen')
+    }
+  }
   render () {
-    const { navigation } = this.props
-    return <HomeActionButtons navigation={navigation} />
+    const { navigation, actions: { logoutUser } } = this.props
+    return <HomeActionButtons navigation={navigation} logout={logoutUser} />
   }
 }
